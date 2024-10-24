d9e4e5b481749aba60d689c77560539a0b02e833
Quoc Binh <Quoc Binh@LAPTOP-ONF2C7PR>
2024-10-24 14:04:25 +0700

    Add project files.

---
+++ BaiTap08/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace BaiTap08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cboMajor = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(405, 179);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 24;
+            this.dgvStudents.Size = new System.Drawing.Size(364, 244);
+            this.dgvStudents.TabIndex = 0;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(176, 179);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(161, 22);
+            this.txtFullName.TabIndex = 1;   
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(177, 263);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 1;
+            // 
+            // cboMajor
+            // 
+            this.cboMajor.FormattingEnabled = true;
+            this.cboMajor.Items.AddRange(new object[] {
+            "CNTT",
+            "NNA",
+            "QTKD"});
+            this.cboMajor.Location = new System.Drawing.Point(176, 327);
+            this.cboMajor.Name = "cboMajor";
+            this.cboMajor.Size = new System.Drawing.Size(101, 24);
+            this.cboMajor.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(30, 400);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(144, 400);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(251, 400);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "sữa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Họ và Tên";
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tuổi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ngành";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(334, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Quản Lý Sinh Viên";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cboMajor);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.dgvStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cboMajor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BaiTap08/Form1.cs
@@ -0,0 +1,141 @@
+﻿using BaiTap08.Modell1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            using (var context = new SchoolDBEntities())
+            {
+                dgvStudents.DataSource = context.Students.ToList();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var context = new SchoolDBEntities())
+            {
+                var student = new Student
+                {
+                    FullName = txtFullName.Text,
+                    Age = Convert.ToInt32(txtAge.Text),
+                    Major = cboMajor.Text                           
+                };
+                context.Students.Add(student);
+                context.SaveChanges();
+                LoadData();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.CurrentRow != null)
+            {
+                int studentId = Convert.ToInt32(dgvStudents.CurrentRow.Cells["StudentId"].Value);
+                using (var context = new SchoolDBEntities())
+                {
+                    var student = context.Students.Find(studentId);
+                    if (student != null)
+                    {
+                        student.FullName = txtFullName.Text;
+                        student.Age = Convert.ToInt32(txtAge.Text);
+                        student.Major = cboMajor.Text;
+                        context.SaveChanges();
+                        LoadData();
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ensure the StudentId column exists
+                if (dgvStudents.Columns.Contains("StudentId"))
+                {
+                    // Ensure a row is selected
+                    if (dgvStudents.SelectedRows.Count > 0)
+                    {
+                        // Confirm deletion with the user
+                        DialogResult result = MessageBox.Show("Are you sure you want to delete this student?",
+                                                              "Confirm Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.Yes)
+                        {
+                            // Get the StudentId of the selected row
+                            int studentId = Convert.ToInt32(dgvStudents.SelectedRows[0].Cells["StudentId"].Value);
+
+                            using (var context = new SchoolDBEntities())
+                            {
+                                var student = context.Students.Find(studentId);
+                                if (student != null)
+                                {
+                                    // Remove the student and save changes
+                                    context.Students.Remove(student);
+                                    context.SaveChanges();
+
+                                    // Reload the data to reflect changes
+                                    LoadData();
+
+                                    MessageBox.Show("Student deleted successfully.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Student not found in the database.");
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a student to delete.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Column 'StudentId' does not exist in the DataGridView.");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid StudentId format.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+
+        private void dgvStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvStudents.CurrentRow != null)
+            {
+                txtFullName.Text = dgvStudents.CurrentRow.Cells["FullName"].Value.ToString();
+                txtAge.Text = dgvStudents.CurrentRow.Cells["Age"].Value.ToString();
+                cboMajor.Text = dgvStudents.CurrentRow.Cells["Major"].Value.ToString();
+            }
+        }
+    }
+}
